45ac9ed7d999337171faf13350916b977bff3f89
Jefferson0k <jefersoncovenas7@gmail.com>
2025-06-23 18:59:20 -0500

    ok

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,16 @@
-
 <template>
     <Breadcrumb :home="home" :model="items" />
+    <div class="card mb-6" style="background-color: #4F91FF; border-radius: 16px;">
+        <h2 class="text-black">Bienvenido [Jeferson] a tu perfil del inversionista</h2>
+    </div>
+    <div class="grid grid-cols-12 gap-4">
+        <StatsWidget />
+    </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import StatsWidget from '@/components/dashboard/StatsWidget.vue';
 
 const home = ref({
     icon: 'pi pi-home'
