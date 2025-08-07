bd9acee080c16b1e01bf85fc9f702987827cd759
Jefferson0k <jefersoncovenas7@gmail.com>
2025-08-07 14:31:06 -0500

    modificacion a la ruta

--- src/services/visitaService.js
+++ src/services/visitaService.js
@@ -3,6 +3,7 @@ import { apiAdmin1 } from './api.js'
 
 export const visitaService = {
   registrarVisita(productoId) {
+    // Agregamos /api/ al inicio de la URL ya que la ruta está en api.php
     return apiAdmin1.post(`/producto/${productoId}/click`)
   }
 }
